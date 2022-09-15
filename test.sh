526efb6a745284d5f2374fe23a1c386336a87357
Marius Hoch <hoo@online.de>
2022-09-15 13:49:36 +0200

    test.sh: Test that README usage excerpt is up to date

--- test.sh
+++ test.sh
@@ -10,4 +10,9 @@ if bwrap --ro-bind / / true 2>/dev/null && test -w /dev/fuse && test -c /dev/fus
 	bwrap --dev-bind / / --ro-bind /etc/hostname /dev/fuse bats transient-file-view.bats
 fi
 
+echo 'Checking whether the usage instructions in the README are up to date.'
+# Check without spaces, tabs, newlines and the line containing the version number
+cat README.md | tr -d '\t\n ' | grep -qF "$(./transient-file-view --help | tail -n +2 | tr -d '\t\n ')" /dev/stdin
+echo
+
 echo Success
